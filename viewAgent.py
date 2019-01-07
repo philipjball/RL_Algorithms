e0c67ef1c38e93d1f997662057e6f84c09c733b1
Philip Ball <t-phball@microsoft.com>
2019-01-07 15:42:05 +0000

    visualise vizdoom basic agent

--- viewAgent.py
+++ viewAgent.py
@@ -1,12 +1,6 @@
 import argparse
 import os
-
-
-def check_env(value):
-    if value in ['FlappyBird-v0', 'CartPole-v0']:
-        return value
-    else:
-        raise argparse.ArgumentTypeError("%s is not a gym environment" % value)
+from utils.helpers import *
 
 
 def main(args):
@@ -15,14 +9,25 @@ def main(args):
             testfile = args.testfile
         else:
             testfile = './params/trained_params_gym_cp2.pth'
-        os.system('python runExp.py --env ' + args.env + ' --frame_stack 1 --testfile ' + testfile +
-                  ' --num_episodes ' + str(args.num_episodes) + ' --frame_skip 1')
+        command_str = 'python runExp.py --env ' + args.env + ' --frame_stack 1 --testfile ' + testfile\
+                      + ' --num_episodes ' + str(args.num_episodes) + ' --frame_skip 1'
     elif args.env == 'FlappyBird-v0':
         if args.testfile:
             testfile = args.testfile
         else:
             testfile = './params/trained_params_gym_fb.pth'
-        os.system('python runExp.py --testfile ' + testfile)
+        command_str = 'python runExp.py --testfile ' + testfile + ' --num_episodes ' + str(args.num_episodes)
+    elif args.env == 'VizdoomBasic-v0':
+        if args.testfile:
+            testfile = args.testfile
+        else:
+            testfile = './params/trained_params_gym_vizdoombasic.pth'
+        command_str = 'python runExp.py --testfile ' + testfile + ' --env ' + args.env + ' --num_episodes '\
+                      + str(args.num_episodes)
+
+    if args.visualise:
+        command_str +=  ' --visualise True'
+    os.system(command_str)
 
 
 if __name__ == '__main__':
